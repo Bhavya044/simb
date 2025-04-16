23d988e5b04032f8bf7eb8826f389badc3201d8c
Bhavya044 <bhavya0381.cse19@chitkara.edu.in>
2025-04-16 22:54:43 +0530

    fix/resolve build errors

--- src/components/UI/Icons/TerminalIcon.tsx
+++ src/components/UI/Icons/TerminalIcon.tsx
@@ -5,7 +5,7 @@ interface TickIconProps {
     w?: number | string;
 }
 
-export const TerminalIcon: React.FC<TickIconProps> = ({ color, h, w }) => (
+export const TerminalIcon: React.FC<TickIconProps> = ({ color }) => (
     <svg className="w-4 h-4 text-gray-400" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M4 17L10 11L4 5" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
         <path d="M12 19H20" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,44 +1,12 @@
-"use client"
+"use client";
 
-import { motion, AnimatePresence } from "framer-motion";
-import WithoutSimbian from "@/components/sections/WithoutSimbian";
-import WithSimbian from "@/components/sections/WithSimbian";
-import HeroSection from "../components/HeroSection";
 import BackgroundWrapper from "../components/BackgroundWrapper";
-import { useVisualMode } from "@/hooks/useVisualMode";
 import SimbianComparison from "../components/sections/SimbianComparison";
 
 export default function HomePage() {
-
-  const visualMode = useVisualMode();
-
   return (
     <BackgroundWrapper>
       <SimbianComparison />
-
-      {/* <AnimatePresence mode="wait">
-        {visualMode === "withoutSimbian" ? (
-          <motion.section
-            key="without"
-            initial={{ x: 0 }}
-            exit={{ x: "-100%" }}
-            transition={{ duration: 0.8, ease: "easeInOut" }}
-            className="min-h-screen"
-          >
-            <WithoutSimbian />
-          </motion.section>
-        ) : (
-          <motion.section
-            key="with"
-            initial={{ x: "100%" }}
-            animate={{ x: 0 }}
-            transition={{ duration: 0.8, ease: "easeInOut" }}
-            className="min-h-screen"
-          >
-            <WithSimbian />
-          </motion.section>
-        )}
-      </AnimatePresence> */}
     </BackgroundWrapper>
   );
 }
